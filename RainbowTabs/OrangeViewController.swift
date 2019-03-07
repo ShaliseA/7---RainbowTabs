daefe79796ab7f3c732a28a26e9e14c6fa3debde
Shalise Ayromloo <shaliseayromloo@shalises-macbook-air.local>
2019-03-06 18:27:46 -0600

    This app is for practicing building a Tab Bar Interface.

---
+++ RainbowTabs/OrangeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OrangeViewController.swift
+//  RainbowTabs
+//
+//  Created by Shalise Ayromloo on 3/5/19.
+//  Copyright © 2019 Shalise Ayromloo. All rights reserved.
+//
+
+import UIKit
+
+class OrangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
